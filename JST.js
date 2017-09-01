703d3a5e2684bbf23ebfc3e139261989f00ade24
sotazer <31537949+sotazer@users.noreply.github.com>
2017-09-01 18:06:30 +0700

    Add files via upload

---
+++ JST.js
@@ -0,0 +1,8 @@
+$(document).ready(function()
+{
+  var aa=$("#myid").val();
+  if (aa == null) {
+    window.location.href = "http://www.sotazone.net/";
+  };
+ $("#myid").attr("href","http://www.sotazone.net/");
+});
