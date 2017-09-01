a79c770b79a9026c1b6b713672f3fd8750bec1f4
sotazer <31537949+sotazer@users.noreply.github.com>
2017-09-01 18:26:11 +0700

    Delete JST.js

--- JST.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function()
-{
-  var aa=$("#myid").val();
-  if (aa == null) {
-    window.location.href = "http://www.sotazone.net/";
-  };
- $("#myid").attr("href","http://www.sotazone.net/");
-});
